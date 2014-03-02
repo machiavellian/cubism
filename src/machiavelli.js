070fc08201789c34da29962bd20656d2e2754a81
Katie McLaughlin <katie@anchor.net.au>
2014-03-03 10:38:05 +1100

    Add Machiavelli context
    "Generic" json feed metric source for [anchor/machiavelli]. Can be altered to
    suit any generic json feed.

---
+++ src/machiavelli.js
@@ -0,0 +1,33 @@
+cubism_contextPrototype.machiavelli= function(host) {
+  if (!arguments.length) host = "";
+  var source = {},
+      context = this;
+
+  source.metric = function(target) {
+
+    var metric = context.metric(function(start, stop, step, callback) {
+
+	feed = host + "/metrics/"
+	      + target
+	      + "?start=" + cubism_machiavelliFormatDate(start - 2 * step)
+	      + "&end=" + cubism_machiavelliFormatDate(stop - 1000)
+	      + "&step="+ step/1000
+	d3.json(feed
+          , function(data) {
+          if (!data) return callback(new Error("unable to load data"));
+          callback(null, data.map(function(d) { return d.y} ))
+      });
+    }, target += "");
+    return metric;
+  };
+
+  source.toString = function() {
+    return host;
+  };
+
+  return source;
+};
+
+function cubism_machiavelliFormatDate(time) {                                                                                                                                        
+  return Math.floor(time / 1000);
+}
