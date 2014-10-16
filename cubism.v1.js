129d502ccccba9c08fb855e1589cb71b74e36541
Katie McLaughlin <katie@glasnt.com>
2014-10-16 11:12:09 +1100

    compile assests

--- cubism.v1.js
+++ cubism.v1.js
@@ -491,8 +491,8 @@ cubism_contextPrototype.machiavelli= function(host) {
 	      + "&step="+ step/1000
 	d3.json(feed
           , function(data) {
-          if (!data || data.length == 0) return callback(new Error("error loading data - no data returned"));
-          if (data.error) return callback(new Error("machiavelli error: "+data.error));
+          if (!data || data.length == 0) return callback(machiavelli_error(target,"error loading data - no data returned"));
+          if (data.error) return callback(machiavelli_error(target,data.error));
           callback(postEffect(), data.map(function(d) { return d.y} ))
       });
     }, metricName);
@@ -508,6 +508,12 @@ cubism_contextPrototype.machiavelli= function(host) {
 
 function cubism_machiavelliFormatDate(time) {                                                                                                                                        
   return Math.floor(time / 1000);
+}
+
+function machiavelli_error(target,msg) {
+        var t = $.grep(chart.metric, function(i,d){ return i.id == target})[0].title //this wont work outside clizia
+        $("span:contains('"+t+"')").filter(".title").append(" -- "+msg);
+        new Error(msg);
 }
 cubism_contextPrototype.gangliaWeb = function(config) {
   var host = '',
